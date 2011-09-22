78c456038ca8a24597efca24e3df755d35ea768f
Matt Kangas <matt@daylife.com>
2011-09-22 17:18:06 -0400

    update minbox-ops cookbook: create users from databag

--- cookbooks/minbox-ops/metadata.rb
+++ cookbooks/minbox-ops/metadata.rb
@@ -4,3 +4,11 @@ license          "Apache 2.0"
 description      "Installs/Configures minbox-ops"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+%w{ ubuntu debian centos redhat fedora }.each do |os|
+  supports os
+end
+
+%w{ sudo users }.each do |cookbook|
+  depends cookbook
+end
--- cookbooks/minbox-ops/recipes/default.rb
+++ cookbooks/minbox-ops/recipes/default.rb
@@ -16,3 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+include_recipe "sudo"
+include_recipe "users::sysadmins"
